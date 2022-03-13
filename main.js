901864946d33f722bb29d939a03b12997d14e47c
Denis Weber <denisweber@hotmail.com>
2022-03-13 11:53:46 -0300

    Implementado a opção de carrossel na página

--- main.js
+++ main.js
@@ -1,28 +1,48 @@
 const imagensAnimes = document.querySelectorAll('.imagens-anime');
+const videosAnimes = document.querySelectorAll('.videos');
+const containerImagens = document.querySelector('.containerImagens');
+const descricaoAnimes = document.querySelectorAll('.descricao');
+let indice = 0;
 
+//função responsável pelo carrossel
+function carrossel(){
+
+    indice++;
+
+    if (indice > imagensAnimes.length - 1){
+        indice = 0;
+    }
+    
+    containerImagens.style.transform = `translateX(${-indice * 560}px)`;
+}
+
+//função que determina um intervalo para a execução da função 'carrossel'
+setInterval(carrossel, 2500);
+
+
+//recebe como parametro o nome do anime que irá exibir o vídeo e a descrição
 function exibirInformacoes(nomeAnime){
     document.querySelector(`.sobre-${nomeAnime}`).removeAttribute('hidden');
     document.querySelector(`.video-${nomeAnime}`).removeAttribute('hidden');
 }
 
-function ocultarInformacoes(nomeAnime){
-    document.querySelector(`.sobre-${nomeAnime}`).setAttribute('hidden', 'hidden');
-    document.querySelector(`.video-${nomeAnime}`).setAttribute('hidden', 'hidden');
+//oculta a descrição e vídeo de todos os animes
+function ocultarInformacoes() {
+    for (let i = 0; i < imagensAnimes.length; i++){
+        descricaoAnimes[i].setAttribute('hidden', 'hidden');
+        videosAnimes[i].setAttribute('hidden', 'hidden');
+    }
 }
 
+//monitora o evento de click nas imagens e exibe suas informações na página
 for (let i = 0; i < imagensAnimes.length; i++){
+
     const anime = imagensAnimes[i];
-    let ativo = 0;
 
     anime.addEventListener('click', function(){
-        if (ativo == 0){
+
+        ocultarInformacoes();
         exibirInformacoes(anime.classList[1]);
-        ativo++;
-        }
-        else {
-            ocultarInformacoes(anime.classList[1]);
-            ativo--;
-        }
     })
 }
 
